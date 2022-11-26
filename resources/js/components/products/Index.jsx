89f1a51977e5aa9974faa37aeb7a9f1a4e9bd2e5
karenciaAO <karenaraqueo@gmail.com>
2022-11-25 19:02:59 -0500

    first commit

---
+++ resources/js/components/products/Index.jsx
@@ -0,0 +1,121 @@
+import React, { useEffect, useState} from 'react'
+import { useNavigate } from 'react-router-dom'
+//import { useEffect, useState } from 'react'
+import axios from 'axios'
+
+const Index = () =>{
+
+    const navigate = useNavigate()
+
+    const [product, setProducts]=useState([])
+
+    const newProduct = () => {
+        navigate("/product/new")
+    }
+
+    useEffect(()=>{
+        getProducts()
+
+    },[])
+
+    const getProducts = async () => {
+        await axios.get("/api/get_all_product")
+            .then(({data})=>{
+                setProducts(data.products)
+            })
+
+    }
+
+    const editProduct = (id) => {
+        navigate('/product/edit/'+id)
+    }
+
+    const deleteProduct = async(id)=>{
+        Swal.fire({
+            title:'Are you  sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor:'#d33',
+            confirmButtonText: 'Yes, delete it!'
+
+
+        })
+
+        .then((result)=>{
+            if(result.isConfirmed){
+                axios.get('/api/delete_product/'+id)
+                    .then(()=>{
+                        Swal.fire(
+                            'Deleted!',
+                            'Product successfully deleted',
+                            'success'
+                        )
+                        getProducts()
+                    })
+                    .catch(()=>{
+
+                    })
+            }
+        })
+    }
+
+
+
+
+    return(
+        <div className='container'>
+            <div className="products_list">
+                <div className="titlebar">
+                    <div className="titlebar_item">
+                        <h1>Products</h1>
+                    </div> 
+                    <div className="titlebar_item">
+                        <div className="btn" onClick={()=>newProduct()}>
+                            Add Product
+                        </div>
+                    
+                    </div>                       
+                </div>
+
+                <div className="table">
+                    <div className="list_header">
+                        <p>Image</p>
+                        <p>Product</p>
+                        <p>Type</p>
+                        <p>Inventory</p>
+                        <p>Actions</p>                       
+                    </div>
+                    {
+                        product.length > 0 && (
+                            product.map((item, key)=>(
+                                <div className="list_items" key={key}>
+                                    <img src={`/upload/${item.photo}`} height="40px" />
+                                    <a>{item.name}</a>
+                                    <p>{item.type}</p>
+                                    <p>{item.quantity}</p>
+                                    <div>
+                                        <button className="btn-icon success" onClick={()=>editProduct(item.id)}>
+                                            <i className="fas fa-pencil-alt"></i>
+            
+                                        </button>
+            
+                                        <button className="btn-icon danger" onClick={()=>deleteProduct(item.id)}>
+                                            <i className="far fa-trash-alt"></i>
+                                        </button>
+                                    </div>
+        
+                                </div>
+
+                            ))
+                        )
+                    }
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Index
